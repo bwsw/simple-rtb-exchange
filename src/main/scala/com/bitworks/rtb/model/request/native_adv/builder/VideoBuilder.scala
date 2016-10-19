0b36361b62d8b158d4ff419db0cfa92f8e1e7648
tomskih_pa <tomskih_pa@bw-sw.com>
2016-10-19 16:42:13 +0700

    Ticket #2525. Add TitleBuilder, ImageBuilder, VideoBuilder and tested.

---
+++ src/main/scala/com/bitworks/rtb/model/request/native_adv/builder/VideoBuilder.scala
@@ -0,0 +1,34 @@
+package com.bitworks.rtb.model.request.native_adv.builder
+
+import com.bitworks.rtb.model.request.native_adv.Video
+
+/**
+  *
+  * Created on: 10/19/2016
+  *
+  * @author Tomskih Pavel
+  * @version %I%
+  *
+  * All Rights Reserved (c) 2016 Bitworks Software, Ltd.
+  *
+  * Builder for [[com.bitworks.rtb.model.request.native_adv.Video]]
+  */
+class VideoBuilder private {
+  private var mimes: Option[Seq[String]] = None
+  private var minDuration: Option[Int] = None
+  private var maxDuration: Option[Int] = None
+  private var protocols: Option[Seq[Int]] = None
+  private var ext: Option[Any] = None
+
+  def withMimes(s: Seq[String]) = { mimes = Some(s); this }
+  def withMinDuration(i: Int) = { minDuration = Some(i); this }
+  def withMaxDuration(i: Int) = { maxDuration = Some(i); this }
+  def withProtocols(s: Seq[Int]) = { protocols = Some(s); this }
+  def withExt(a: Any) = { ext = Some(a); this }
+
+  def build = Video(mimes, minDuration, maxDuration, protocols, ext)
+}
+
+object VideoBuilder {
+  def apply() = new VideoBuilder
+}
