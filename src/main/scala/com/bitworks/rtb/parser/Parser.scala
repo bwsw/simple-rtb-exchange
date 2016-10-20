4066bf0b3c2ed4a41e82fb17cd460ba4c3f1f190
tomskih_pa <tomskih_pa@bw-sw.com>
2016-10-20 18:28:00 +0700

    Add DataValidationException, trait Parser.

---
+++ src/main/scala/com/bitworks/rtb/parser/Parser.scala
@@ -0,0 +1,16 @@
+package com.bitworks.rtb.parser
+
+import org.json4s.JsonInput
+
+/** Interface for parsers.
+  *
+  * Created on: 10/20/2016
+  *
+  * @author Tomskih Pavel
+  * @version %I%
+  *
+  *          All Rights Reserved (c) 2016 Bitworks Software, Ltd.
+  */
+trait Parser {
+  def parse(in: JsonInput)
+}
