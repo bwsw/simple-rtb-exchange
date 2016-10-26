644fe93eca1b3e80feb30d8708dec45b3e9b5820
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2016-10-26 17:12:13 +0700

    Ticket #2456. Implement JSON parser for BidResponse.

---
+++ src/main/scala/com/bitworks/rtb/parser/response/BidResponseParser.scala
@@ -0,0 +1,36 @@
+package com.bitworks.rtb.parser.response
+
+import com.bitworks.rtb.model.response.BidResponse
+import com.bitworks.rtb.parser.DataValidationException
+
+/**
+  * Interface for [[com.bitworks.rtb.model.response.BidResponse BidResponse]] parsers.
+  *
+  * @author Pavel Tomskikh
+  */
+trait BidResponseParser {
+
+  /**
+    * Returns parsed string to [[com.bitworks.rtb.model.response.BidResponse BidResponse]] object.
+    *
+    * @param s parsing string
+    * @throws DataValidationException in case of invalid JSON
+    */
+  def parse(s: String): BidResponse = {
+    try {
+      parseInternal(s)
+    }
+    catch {
+      case e: DataValidationException => throw e
+      case e: Throwable => throw new DataValidationException(cause = e)
+    }
+  }
+
+  /**
+    * Returns parsed string to [[com.bitworks.rtb.model.response.BidResponse BidResponse]] object
+    * without exception catching.
+    *
+    * @param s parsing string
+    */
+  protected def parseInternal(s: String): BidResponse
+}
