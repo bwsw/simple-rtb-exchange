3ff80c9bccb75dd90d002027967b3f4d7a9f9bfb
Egor Ilchenko <ilchenko_ei@bw-sw.com>
2016-11-07 12:00:45 +0700

    Ticket #2602.
    Added actor for cache updating

---
+++ src/main/scala/com/bitworks/rtb/service/actor/CacheUpdaterActor.scala
@@ -0,0 +1,24 @@
+package com.bitworks.rtb.service.actor
+
+import akka.actor.{Actor, ActorLogging}
+import com.bitworks.rtb.service.dao.CacheUpdater
+import com.bitworks.rtb.model.message._
+
+/**
+  * Actor for cache updating.
+  *
+  * @author Egor Ilchenko
+  */
+class CacheUpdaterActor(dbUpdater: CacheUpdater) extends Actor with ActorLogging{
+  override def receive: Receive = {
+    case InitCache =>
+      log.debug("cache initializing started...")
+      dbUpdater.notifyAll(InitCache)
+      log.debug("cache initializing finished")
+    case UpdateCache =>
+      log.debug("cache updating started...")
+      dbUpdater.notifyAll(UpdateCache)
+      log.debug("cache updating finished")
+    case _ =>
+  }
+}
