6f5ef7b398ec3bb9c2d18b9b22849d96a47fed41
Pavel Tomskikh <tomskih_pa@bw-sw.com>
2016-11-11 11:09:04 +0700

    Ticket #2604. Add Validator for BidResponse according to BidRequest.

--- src/main/scala/com/bitworks/rtb/validator/BidResponseValidator.scala
+++ src/main/scala/com/bitworks/rtb/validator/BidResponseValidator.scala
@@ -1,59 +1,168 @@
 package com.bitworks.rtb.validator
 
+import com.bitworks.rtb.model.request.{App, BidRequest, Imp}
 import com.bitworks.rtb.model.response._
 
 /**
-  * Validator for [[com.bitworks.rtb.model.response.BidResponse BidResponse]]
+  * Validator for [[com.bitworks.rtb.model.response.BidResponse BidResponse]] according to
+  * [[com.bitworks.rtb.model.request.BidRequest BidRequest]] object.
   *
+  * @param bidRequest [[com.bitworks.rtb.model.request.BidRequest BidRequest]] object
   * @author Pavel Tomskikh
   */
-class BidResponseValidator {
+class BidResponseValidator(bidRequest: BidRequest) {
 
   /**
-    * Returns true if [[com.bitworks.rtb.model.response.BidResponse BidResponse]] object is valid.
+    * Returns Some([[com.bitworks.rtb.model.response.BidResponse BidResponse]]) if bidResponse is
+    * valid, or None else.
     *
-    * @param bidResponse [[com.bitworks.rtb.model.response.BidResponse BidResponse]] object
+    * @param bidResponse validated [[com.bitworks.rtb.model.response.BidResponse BidResponse]]
+    *                    object
     */
-  def validate(bidResponse: BidResponse): Boolean = {
-    bidResponse.id.nonEmpty &&
-      (bidResponse.seatBid.nonEmpty &&
-        bidResponse.seatBid.forall(validateSeatBid) ||
-        bidResponse.nbr.nonEmpty) &&
+  def validate(bidResponse: BidResponse): Option[BidResponse] = {
+    if (bidResponse.id == bidRequest.id &&
       bidResponse.bidId.forall(_.nonEmpty) &&
-      bidResponse.customData.forall(_.nonEmpty)
+      bidResponse.cur.nonEmpty &&
+      bidResponse.customData.forall(_.nonEmpty)) {
+
+      var seatBids: Seq[SeatBid] = bidResponse.seatBid
+
+      if (bidResponse.nbr.isEmpty) {
+        seatBids = bidResponse.seatBid.map(validateSeatBid).filter(_.nonEmpty).map(_.get)
+        if (seatBids.isEmpty) return None
+      }
+
+      Some(
+        BidResponse(
+          bidResponse.id,
+          seatBids,
+          bidResponse.bidId,
+          bidResponse.cur,
+          bidResponse.customData,
+          bidResponse.nbr,
+          bidResponse.ext))
+    }
+
+    else None
   }
 
-  /**
-    * Returns true if [[com.bitworks.rtb.model.response.SeatBid SeatBid]] object is valid.
-    *
-    * @param seatBid [[com.bitworks.rtb.model.response.SeatBid SeatBid]] object
-    */
-  private def validateSeatBid(seatBid: SeatBid): Boolean = {
-    seatBid.bid.nonEmpty &&
-      seatBid.bid.forall(validateBid) &&
-      Seq(0, 1).contains(seatBid.group)
+  private def validateSeatBid(seatBid: SeatBid): Option[SeatBid] = {
+    seatBid.group match {
+      case 0 =>
+        val bids = seatBid.bid.filter(validateBid(bidRequest.imp, seatBid.seat))
+        if (bids.nonEmpty) Some(SeatBid(bids, seatBid.seat, seatBid.group, seatBid.ext))
+        else None
+      case 1 =>
+        if (seatBid.bid.forall(validateBid(bidRequest.imp, seatBid.seat))) Some(seatBid)
+        else None
+      case _ => None
+    }
   }
 
-  /**
-    * Returns true if [[com.bitworks.rtb.model.response.Bid Bid]] object is valid.
-    *
-    * @param bid [[com.bitworks.rtb.model.response.Bid Bid]] object
-    */
-  private def validateBid(bid: Bid): Boolean = {
-    bid.id.nonEmpty &&
-      bid.impId.nonEmpty &&
-      bid.price > 0 &&
-      bid.adId.forall(_.nonEmpty) &&
-      bid.nurl.forall(_.nonEmpty) &&
-      bid.adm.forall(_.nonEmpty) &&
-      bid.adomain.forall(_.forall(_.nonEmpty)) &&
-      bid.bundle.forall(_.nonEmpty) &&
-      bid.iurl.forall(_.nonEmpty) &&
-      bid.cid.forall(_.nonEmpty) &&
-      bid.crid.forall(_.nonEmpty) &&
-      bid.cat.forall(_.forall(_.nonEmpty)) &&
-      bid.dealId.forall(_.nonEmpty) &&
-      bid.h.forall(_ > 0) &&
-      bid.w.forall(_ > 0)
+  private def validateBid(imps: Seq[Imp], seat: Option[String])(bid: Bid): Boolean = {
+    imps.find(_.id == bid.impId) match {
+      case Some(imp) =>
+        bid.id.nonEmpty && (
+          bid.dealId match {
+            case Some(dealId) => validateBidWithDeal(imp, seat, bid)
+            case None => validateBidWithoutDeal(imp, seat, bid)
+          }) &&
+          bid.adId.forall(_.nonEmpty) &&
+          bid.nurl.forall(_.nonEmpty) &&
+          bid.adm.forall(_.nonEmpty) &&
+          bidRequest.app.forall(checkBundle(bid.bundle)) &&
+          bid.iurl.forall(_.nonEmpty) &&
+          bid.cid.forall(_.nonEmpty) &&
+          bid.crid.forall(_.nonEmpty) &&
+          isAllNotInBlackList(bid.cat, bidRequest.bcat) &&
+          checkSize(bid.h, bid.w, imp)
+      case None => false
+    }
   }
+
+  private def checkSize(h: Option[Int], w: Option[Int], imp: Imp): Boolean = {
+    if (imp.banner.nonEmpty) {
+      val banner = imp.banner.get
+      checkOneDimension(h, banner.h, banner.hmin, banner.hmax) &&
+        checkOneDimension(w, banner.w, banner.wmin, banner.wmax)
+    }
+    else if (imp.video.nonEmpty) {
+      val video = imp.video.get
+      (video.h.isEmpty || video.h == h) &&
+        (video.w.isEmpty || video.w == w)
+    }
+    else true
+  }
+
+  private def checkOneDimension(
+      dimension: Option[Int],
+      expected: Option[Int],
+      min: Option[Int],
+      max: Option[Int]): Boolean = {
+    if (dimension.nonEmpty) {
+      expected.nonEmpty && dimension.get == expected.get ||
+        (min.isEmpty || dimension.get >= min.get) &&
+          (max.isEmpty || dimension.get <= max.get)
+    }
+    else {
+      expected.isEmpty &&
+        max.isEmpty &&
+        min.isEmpty
+    }
+  }
+
+  private def checkBundle(bundle: Option[String])(app: App): Boolean = {
+    app.bundle.forall { b =>
+      bundle.nonEmpty &&
+        b == bundle.get
+    }
+  }
+
+  private def validateBidWithDeal(
+      imp: Imp,
+      seat: Option[String],
+      bid: Bid): Boolean = {
+    imp.pmp.nonEmpty &&
+      imp.pmp.get.deals.nonEmpty && (
+      imp.pmp.get.deals.get.find(_.id == bid.dealId.get) match {
+        case Some(deal) =>
+          bid.price >= deal.bidFloor && (
+            bid.adomain.isEmpty ||
+              isLeastOneInWhiteList(bid.adomain, deal.wadomain) ||
+              isLeastOneNotInBlackList(bid.adomain, bidRequest.badv)) && (
+            deal.wseat.isEmpty ||
+              seat.nonEmpty &&
+                deal.wseat.get.contains(seat.get))
+        case None => false
+      })
+  }
+
+  private def validateBidWithoutDeal(imp: Imp, seat: Option[String], bid: Bid): Boolean = {
+    bid.price >= imp.bidFloor && (
+      bid.adomain.isEmpty ||
+        isLeastOneNotInBlackList(bid.adomain, bidRequest.badv))
+  }
+
+  private def isLeastOneInWhiteList[T](
+      items: Option[Seq[T]],
+      whiteList: Option[Seq[T]]): Boolean = {
+    whiteList.isEmpty ||
+      items.nonEmpty &&
+        whiteList.get.intersect(items.get).nonEmpty
+  }
+
+  private def isLeastOneNotInBlackList[T](
+      items: Option[Seq[T]],
+      blackList: Option[Seq[T]]): Boolean = {
+    blackList.isEmpty ||
+      items.nonEmpty &&
+        items.get.exists(!blackList.get.contains(_))
+  }
+
+  private def isAllNotInBlackList[T](items: Option[Seq[T]], blackList: Option[Seq[T]]): Boolean = {
+    blackList.isEmpty ||
+      items.isEmpty ||
+      !items.get.exists(blackList.get.contains)
+  }
+
 }
--- src/test/scala/com/bitworks/rtb/validator/BidResponseValidatorTest.scala
+++ src/test/scala/com/bitworks/rtb/validator/BidResponseValidatorTest.scala
@@ -1,5 +1,6 @@
 package com.bitworks.rtb.validator
 
+import com.bitworks.rtb.model.request.builder._
 import com.bitworks.rtb.model.response.builder._
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -10,89 +11,160 @@ import org.scalatest.{FlatSpec, Matchers}
   */
 class BidResponseValidatorTest extends FlatSpec with Matchers {
 
-  val validator = new BidResponseValidator
+  val seat = "seat1"
+  val deal = DealBuilder("deal1243")
+    .withBidFloor(1.2)
+    .withWadomain(Seq("good.com"))
+    .withWseat(Seq(seat))
+    .build
+  val pmp = PmpBuilder().withDeals(Seq(deal)).build
+  val app = AppBuilder().withBundle("app.com").build
+  val banner = BannerBuilder()
+    .withHmax(400)
+    .withHmin(200)
+    .withW(150)
+    .build
+  val imp = ImpBuilder("13512532")
+    .withBidFloor(deal.bidFloor)
+    .withBanner(banner)
+    .withPmp(pmp)
+    .build
+  val bidRequest = BidRequestBuilder("19875198", Seq(imp))
+    .withBcat(Seq("IAB7-3"))
+    .withBadv(Seq("bad.com"))
+    .build
+
+  val validator = new BidResponseValidator(bidRequest)
+
+  val correctBid = BidBuilder(
+    "1",
+    imp.id,
+    deal.bidFloor + 0.1)
+    .withAdId("adid")
+    .withNurl("nurl")
+    .withAdm("adm")
+    .withAdomain(deal.wadomain.get)
+    .withBundle(app.bundle.get)
+    .withCat(Seq("IAB2-2"))
+    .withAttr(Set(1, 3, 5))
+    .withH(banner.hmin.get + 10)
+    .withW(banner.w.get)
+    .withDealId(deal.id)
+    .build
+  val correctSeatBid = SeatBidBuilder(Seq(correctBid))
+    .withSeat(seat)
+    .withGroup(1)
+    .build
 
   "BidResponseValidator" should "validate correct BidResponse" in {
-    val bid = BidBuilder("id", "impid", BigDecimal(42.42))
-      .withAdId("adid")
-      .withNurl("nurl")
-      .withAdm("adm")
-      .withAdomain(Seq("adomain"))
-      .withBundle("bundle")
-      .withIurl("iurl")
-      .withCid("cid")
-      .withCrid("crid")
-      .withCat(Seq.empty)
-      .withAttr(Set(1))
-      .withDealId("dealid")
-      .withH(42)
-      .withW(12)
-      .build
-
-    val seatBid = SeatBidBuilder(Seq(bid))
-      .withSeat("seat")
-      .withGroup(1)
-      .build
-
-    val bidResponse = BidResponseBuilder("123", Seq(seatBid))
-      .withBidId("456")
-      .withCur("EUR")
-      .withCustomData("custom")
-      .build
-
-    validator.validate(bidResponse) shouldBe true
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq(correctSeatBid)).build
+    validator.validate(bidResponse) shouldBe Some(bidResponse)
   }
 
   it should "validate correct not bidded BidResponse" in {
-    val bidResponse = BidResponseBuilder("123", Seq.empty)
-      .withNbr(1)
-      .build
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq.empty).withNbr(1).build
 
-    validator.validate(bidResponse) shouldBe true
-  }
-
-  val correctBid = BidBuilder("123", "456", BigDecimal(42.42)).build
-  val correctSeatBid = SeatBidBuilder(Seq(correctBid)).build
-
-  it should "validate correct BidResponse with default values" in {
-    val bidResponse = BidResponseBuilder("123", Seq(correctSeatBid)).build
-
-    validator.validate(bidResponse) shouldBe true
+    validator.validate(bidResponse) shouldBe Some(bidResponse)
   }
 
   it should "not validate BidResponse with empty id" in {
-    val incorrectBidResponse = BidResponseBuilder("", Seq(correctSeatBid)).build
+    val incorrectBidResponse = BidResponseBuilder("fh2i8", Seq(correctSeatBid)).build
 
-    validator.validate(incorrectBidResponse) shouldBe false
+    validator.validate(incorrectBidResponse) shouldBe None
   }
 
   it should "not validate BidResponse without seatBids and nbr" in {
-    val incorrectBidResponse = BidResponseBuilder("123", Seq.empty).build
+    val incorrectBidResponse = BidResponseBuilder(bidRequest.id, Seq.empty).build
 
-    validator.validate(incorrectBidResponse) shouldBe false
+    validator.validate(incorrectBidResponse) shouldBe None
   }
 
   it should "not validate BidResponse without Bids in SeatBid" in {
     val incorrectSeatBid = SeatBidBuilder(Seq.empty).build
-    val incorrectBidResponse = BidResponseBuilder("123", Seq(incorrectSeatBid)).build
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq(incorrectSeatBid)).build
 
-    validator.validate(incorrectBidResponse) shouldBe false
+    validator.validate(bidResponse) shouldBe None
   }
 
   it should "not validate BidResponse with incorrect group in SeatBid" in {
     val incorrectSeatBid = SeatBidBuilder(Seq(correctBid))
       .withGroup(2)
       .build
-    val incorrectBidResponse = BidResponseBuilder("123", Seq(incorrectSeatBid)).build
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq(incorrectSeatBid)).build
 
-    validator.validate(incorrectBidResponse) shouldBe false
+    validator.validate(bidResponse) shouldBe None
   }
 
-  it should "not validate BidResponse with negative price" in {
-    val incorrectBid = BidBuilder("123", "456", BigDecimal(-1)).build
-    val incorrectSeatBid = SeatBidBuilder(Seq(incorrectBid)).build
-    val incorrectBidResponse = BidResponseBuilder("123", Seq(incorrectSeatBid)).build
+  it should "not validate BidResponse with small price" in {
+    val incorrectBid = BidBuilder("1", imp.id, imp.bidFloor - 0.01).build
+    val seatBid = SeatBidBuilder(Seq(incorrectBid)).build
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq(seatBid)).build
 
-    validator.validate(incorrectBidResponse) shouldBe false
+    validator.validate(bidResponse) shouldBe None
+  }
+
+  it should "remove incorrect Bids from SeatBid when SeatBid.group = 0" in {
+    val incorrectBid = BidBuilder("2", imp.id, -5).build
+    val seatBid = SeatBidBuilder(Seq(incorrectBid, correctBid))
+      .withGroup(0)
+      .withSeat(seat)
+      .build
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq(seatBid)).build
+
+    val expectedSeatBid = SeatBidBuilder(Seq(correctBid)).withSeat(seat).withGroup(0).build
+    val expectedBidResponse = BidResponseBuilder(bidRequest.id, Seq(expectedSeatBid)).build
+
+    validator.validate(bidResponse) shouldBe Some(expectedBidResponse)
+  }
+
+  it should "not validate incorrect Bids when SeatBid.group = 1" in {
+    val incorrectBid = BidBuilder("2", imp.id, imp.bidFloor - 0.01).build
+    val seatBid = SeatBidBuilder(Seq(incorrectBid, correctBid)).withGroup(1).build
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq(seatBid)).build
+
+    validator.validate(bidResponse) shouldBe None
+  }
+
+  it should "not validate BidResponse with blocked categories" in {
+    val incorrectBid = BidBuilder("1", imp.id, imp.bidFloor + 1)
+      .withCat(bidRequest.bcat.get :+ "IAB4-4")
+      .build
+    val seatBid = SeatBidBuilder(Seq(incorrectBid)).build
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq(seatBid)).build
+
+    validator.validate(bidResponse) shouldBe None
+  }
+
+  it should "not validate BidResponse when all domains is blocked" in {
+    val incorrectBid = BidBuilder("1", imp.id, imp.bidFloor + 1)
+      .withAdomain(bidRequest.badv.get)
+      .build
+    val seatBid = SeatBidBuilder(Seq(incorrectBid)).build
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq(seatBid)).build
+
+    validator.validate(bidResponse) shouldBe None
+  }
+
+  it should "validate BidResponse when not all domains is blocked" in {
+    val bid = BidBuilder("1", imp.id, imp.bidFloor + 1)
+      .withAdomain(bidRequest.badv.get :+ "notblocked.com")
+      .withH(banner.hmax.get)
+      .withW(banner.w.get)
+      .build
+    val seatBid = SeatBidBuilder(Seq(bid)).build
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq(seatBid)).build
+
+    validator.validate(bidResponse) shouldBe Some(bidResponse)
+  }
+
+  it should "validate BidResponse without dealId" in {
+    val bid = BidBuilder("1", imp.id, imp.bidFloor + 0.1)
+      .withH(banner.hmax.get)
+      .withW(banner.w.get)
+      .build
+    val seatBid = SeatBidBuilder(Seq(bid)).build
+    val bidResponse = BidResponseBuilder(bidRequest.id, Seq(seatBid)).build
+
+    validator.validate(bidResponse) shouldBe Some(bidResponse)
   }
 }
