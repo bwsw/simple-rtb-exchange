40427b62c7e079bb540dfd73715b0650ade9ca1c
Egor Ilchenko <ilchenko_ei@bw-sw.com>
2016-11-16 17:16:09 +0700

    Ticket #2668.
    Timeout in Bid Response

--- src/main/scala/com/bitworks/rtb/service/actor/RequestActor.scala
+++ src/main/scala/com/bitworks/rtb/service/actor/RequestActor.scala
@@ -1,22 +1,20 @@
 package com.bitworks.rtb.service.actor
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Cancellable, Props}
 import akka.stream.ActorMaterializer
 import com.bitworks.rtb.application.HttpRequestWrapper
 import com.bitworks.rtb.model.ad.response.{AdResponse, Error}
-import com.bitworks.rtb.model.db.Bidder
 import com.bitworks.rtb.model.message.{BidRequestResult, _}
 import com.bitworks.rtb.model.response.BidResponse
-import com.bitworks.rtb.service.{Auction, Configuration}
 import com.bitworks.rtb.service.dao.BidderDao
 import com.bitworks.rtb.service.factory.{AdResponseFactory, BidRequestFactory}
 import com.bitworks.rtb.service.parser.AdRequestParser
 import com.bitworks.rtb.service.writer.AdResponseWriter
+import com.bitworks.rtb.service.{Auction, Configuration}
 import scaldi.Injector
 import scaldi.akka.AkkaInjectable._
 
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.duration._
 
 /**
   * Main actor to process ad requests.
@@ -43,6 +41,8 @@ class RequestActor(
   val bidders = bidderDao.getAll
   val receivedBidResponses = new ListBuffer[BidRequestResult]
 
+  var auctionStartCancellable: Option[Cancellable] = None
+
   override def receive: Receive = {
 
     case HandleRequest =>
@@ -54,13 +54,16 @@ class RequestActor(
           val adRequest = parser.parse(bytes)
           val bidRequest = factory.create(adRequest)
 
-          bidderDao.getAll match {
-            case Seq() => onError("bidders not found")
-            case bidders: Seq[Bidder] =>
-              bidders.foreach { bidder =>
-                bidActor ! SendBidRequest(bidder, bidRequest)
-              }
+          bidderDao.getAll.foreach { bidder =>
+            bidActor ! SendBidRequest(bidder, bidRequest)
           }
+
+          auctionStartCancellable = Some(
+            context.system.scheduler.scheduleOnce(
+              configuration.bidRequestTimeout,
+              self,
+              StartAuction))
+
       } onFailure {
         case exc => onError(exc.toString)
       }
@@ -69,7 +72,7 @@ class RequestActor(
       log.debug(s"bid response received: $msg")
       receivedBidResponses.append(msg)
       if (receivedBidResponses.size == bidders.length)
-        startAuction()
+        self ! StartAuction
 
     case msg: BidResponse =>
       log.debug("bid response received")
@@ -78,26 +81,27 @@ class RequestActor(
 
       completeRequest(response)
 
-  }
+    case StartAuction =>
+      auctionStartCancellable.foreach(_.cancel())
+      log.debug("auction started")
+      val successful = receivedBidResponses
+        .collect {
+          case BidRequestSuccess(response) => response
+        }
+      log.debug(s"auction participants: ${successful.length}")
 
-  def startAuction() = {
-    log.debug("auction started")
-    val successful = receivedBidResponses
-      .collect {
-        case BidRequestSuccess(response) => response
+      val winner = auction.winner(successful)
+      log.debug(s"auction winner: $winner")
+
+      winner match {
+        case Some(response) => winActor ! response
+        case None => onError("winner not defined")
       }
-    log.debug(s"auction participants: ${successful.length}")
 
-    val winner = auction.winner(successful)
-    log.debug(s"auction winner: $winner")
-
-    winner match {
-      case Some(response) => winActor ! response
-      case None => onError("winner not defined")
-    }
   }
 
   def completeRequest(response: AdResponse) = {
+    log.debug("completing request...")
     val bytes = writer.write(response)
     request.complete(bytes)
   }
