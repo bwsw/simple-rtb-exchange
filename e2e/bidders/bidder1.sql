7ee545b4ef18f5b08a46628999719e5c6fc4d5bb
Egor Ilchenko <ilchenko_ei@bw-sw.com>
2016-12-01 17:11:39 +0700

    Ticket #2765.
    Configurable fake-bidder host

--- e2e/bidders/bidder1.sql
+++ e2e/bidders/bidder1.sql
@@ -1,3 +1,4 @@
+DELETE FROM bidder;
 INSERT INTO bidder (name, endpoint) VALUES
     ('bidder1', '{{host}}'),
     ('bidder2', '{{host}}');
--- e2e/query.sh
+++ e2e/query.sh
@@ -2,12 +2,17 @@
 
 # Executes query using psql
 
-if [ "$#" -ne 1 ];
+if [ "$#" -ne 2 ];
 then
-  echo "Usage: $0 query.sql" >&2
+  echo "Usage: $0 fake-bidder-host query.sql" >&2
   exit 1
 fi
 
 export PGPASSWORD="rtb_user"
+script=`cat $2 |tr '\n' ' '`
+replaced_script="${script//\{\{host\}\}/$1}"
 
-psql -h localhost -d rtb_exchange_e2e -U rtb_user -p 5432 -w -q -f $1
+IFS=';'
+for x in $replaced_script; do
+    psql -h localhost -d rtb_exchange_e2e -U rtb_user -p 5432 -w  -c $x
+done
