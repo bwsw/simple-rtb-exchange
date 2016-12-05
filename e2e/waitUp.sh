bfc8c35c51626fb14f30ac1945d0562dc94bb672
Egor Ilchenko <ilchenko_ei@bw-sw.com>
2016-12-05 12:14:52 +0700

    Ticket #2712.
    Added waiting timeout

--- e2e/waitUp.sh
+++ e2e/waitUp.sh
@@ -6,11 +6,17 @@ then
   exit 1
 fi
 
-echo "waiting for rtb exchange to up..."
+RETRIES=60 # connection retries
 
-until [ $(curl --write-out %{http_code} --silent --output /dev/null "$1/definetlynotfound") -eq "404" ]; do
+echo "waiting for rtb exchange to up..."
+until [ $(curl --write-out %{http_code} --silent --output /dev/null "$1/definetly404") -eq "404" ]; do
     printf '.'
+    RETRIES=$((RETRIES - 1))
+    if [ $RETRIES -eq "0" ];
+    then
+        echo "waiting time is over"
+        exit 1
+    fi
     sleep 1
 done
-
 echo "UP!"
