00262a941dc924a5d5e6cb7f125cab49044722fd
Egor Ilchenko <ilchenko_ei@bw-sw.com>
2016-12-07 15:45:15 +0700

    Ticket #2798.
    Tests for Bidders: Win notice

---
+++ e2e/sql/win_bidder_ad_in_winnotice.sql
@@ -0,0 +1,9 @@
+UPDATE bidder SET deleted=true;
+INSERT INTO bidder (name, endpoint) VALUES
+    ('bid1', 'http://{{host}}?price=299'),
+    ('bid2', 'http://{{host}}?price=298'),
+    ('bid3', 'http://{{host}}?price=297'),
+    ('bid4', 'http://{{host}}?price=301&modifier=winnotice-withadm&winhost={{host}}'),
+    ('bid5', 'http://{{host}}?price=296'),
+    ('bid6', 'http://{{host}}?price=295'),
+    ('bid7', 'http://{{host}}?price=294');
